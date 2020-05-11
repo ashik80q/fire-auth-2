784986590434fcf18d71a7dd15e80d00d8c27c85
Ashik khandakar <ashikbpdb@gmail.com>
2020-05-11 10:23:39 +0600

    2nd commit

---
+++ src/firebase.confic.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxK9cXHrmDaRrUpBvnOLiVRSSPmhVjrW8",
+    authDomain: "ema-john-plus.firebaseapp.com",
+    databaseURL: "https://ema-john-plus.firebaseio.com",
+    projectId: "ema-john-plus",
+    storageBucket: "ema-john-plus.appspot.com",
+    messagingSenderId: "9797127440",
+    appId: "1:9797127440:web:044212996fcdfd24ece995"
+  };
+
+  export default firebaseConfig;
